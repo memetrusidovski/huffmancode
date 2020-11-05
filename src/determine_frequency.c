dcef2c310df0ce9e77fa231f96349e2d0a2acfe5
met <64790786+memetrusidovski@users.noreply.github.com>
2020-11-04 22:14:52 -0500

    freqency check

--- src/determine_frequency.c
+++ src/determine_frequency.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <string.h>
+void find_frequency(char [], int []);
+ 
+int main()
+{
+   char string[100];
+   int c, count[26] = {0};
+ 
+   printf("Input a string\n");
+   scanf("%s", string);
+ 
+   find_frequency(string, count);
+   
+   printf("Character Count\n");
+   
+   for (c = 0 ; c < 26 ; c++)
+      printf("%c \t  %d\n", c + 'a', count[c]);
+ 
+   return 0;
+}
 
-int main(void){
-    
+void find_frequency(char s[], int count[]) {
+   int c = 0;
+   
+   while (s[c] != '\0') {
+      if (s[c] >= 'a' && s[c] <= 'z' )
+         count[s[c]-'a']++;
+      c++;
+   }
 }
